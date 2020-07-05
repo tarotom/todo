bd40856eb4c9d796c5303ac4c938a15fc2ab143c
Tomi <ot_tomi@hotmail.com>
2020-07-05 21:47:10 +0300

    Add button for opening the form to create a new Todo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Todo from "./components/Todo";
 
 const App = () => {
+  const handleOpenNewTodoForm = () => {
+    console.log("Opening form for creating a new Todo!");
+  }
+
   return (
     <div>
       <h1>Todos</h1>
+      <button onClick={handleOpenNewTodoForm}>Create new todo</button>
 
       <Todo title="Title of todo 1" description="Description of todo 1"/>
       <Todo title="Title of todo 2" description="Description of todo 2"/>
