a1d11eb07e9766d115be0163a176d241ce78494e
Tomi <ot_tomi@hotmail.com>
2020-07-08 22:40:15 +0300

    Add functionality to open and close Todos edit mode

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = (props) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditButtonClick = () => {
+    setEditing(true);
+  }
+
+  const handleCancelButtonClick = () => {
+    setEditing(false);
+  }
+
+  const renderEditForm = () => {
+    return (
+      <div style={{backgroundColor: "red"}}>
+        <input type="text" value={props.title}/>
+        <button>Save</button>
+        <button onClick={handleCancelButtonClick}>Cancel</button>
+        <input type="text" value={props.description}/>
+        <input type="text" value={props.priority}/>
+      </div>
+    )
+  }
+
+  const renderTodo = () => {
+    return (
+      <div style={{backgroundColor: "red"}}>
+        <h2>{props.title}</h2>
+        <button onClick={handleEditButtonClick}>Edit</button>
+        <p>{props.description}</p>
+        <p>{props.priority}</p>
+      </div>
+    )
+  }
+
   return (
-    <div style={{backgroundColor: "red"}}>
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-      <p>{props.priority}</p>
-    </div>
+    <>
+      { editing ? renderEditForm() : renderTodo() }
+    </>
   )
 }
 
